1ce2daca9807fb9a71c89d9d715dc5e0c27c28bd
TheDoom95 <krauser2011@gmail.com>
2021-07-12 23:01:52 +0200

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, useRef, useState } from 'react';
 
 const App = () => {
-  const [data, setData] = useState<string>('default');
+  const [data, setData] = useState<string>('');
   const [list, setList] = useState<string[]>([]);
   const inputs = useRef<HTMLInputElement[]>([]);
 
@@ -28,28 +28,51 @@ const App = () => {
       <div>
         {
           list.map((text: string, index: number) => (
-            <div key={index + text}>
-              <span>{index}: </span>
+            <div key={index + text} className="mt-5 sm:flex sm:items-center px-4">
+              <span className="mr-2">{index}: </span>
+
               <input
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
                 type="text"
                 defaultValue={text}
                 ref={(input: HTMLInputElement) => inputs.current[index] = input}
               />
 
-              <button onClick={() => edit(index)}>Editar</button>
-              <button onClick={() => remove(index)}>Borrar</button>
+              <button
+                className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={() => edit(index)}
+              >
+                Editar
+              </button>
+
+              <button
+                className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={() => remove(index)}
+              >
+                Borrar
+              </button>
             </div>
           ))
         }
       </div>
 
-      <input
-        placeholder="Introduce una tarea"
-        onChange={handleOnChange}
-        defaultValue={data}
-      />
+      <div className="mt-5 sm:flex sm:items-center px-4">
+        <input
+          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md py-2"
+          placeholder="Introduce una tarea"
+          onChange={handleOnChange}
+          defaultValue={data}
+        />
 
-      <button onClick={save}>Guardar</button>
+        <button
+          className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+          onClick={save}
+        >
+          Guardar
+        </button>
+      </div>
+
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './css/styles.css';
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
